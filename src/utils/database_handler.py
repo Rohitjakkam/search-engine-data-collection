3c955b06f33ccdba5624344dd7b33ee567e43434
Rohitjakkam <rohitjakkam@gmail.com>
2024-03-24 13:08:17 +0530

    fix01

--- src/utils/database_handler.py
+++ src/utils/database_handler.py
@@ -8,8 +8,9 @@ class MongodbClient:
     def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
         if MongodbClient.client is None:
             MongodbClient.client = pymongo.MongoClient(
-                f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+                f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@reverseimagesearch.imkelpv.mongodb.net/?retryWrites=true&w=majority"
             )
         self.client = MongodbClient.client
         self.database = self.client[database_name]
         self.database_name = database_name
+# mongodb+srv://<username>:<password>@reverseimagesearch.imkelpv.mongodb.net/
