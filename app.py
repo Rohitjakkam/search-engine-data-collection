689526085826a4975d4a771cd8a0793905a37819
Rohitjakkam <rohitjakkam@gmail.com>
2024-04-20 13:02:14 +0530

    fix 1113

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def bulk_upload(label: str, files: List[UploadFile] = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
